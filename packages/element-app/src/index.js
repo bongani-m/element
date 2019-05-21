bacfe1f74c44bbd2fff79cae3949d6c7db235481
Bongani Mbigi <bongani@transmute.industries>
2019-05-21 17:13:51 -0500

    moved Tracker to switch

--- packages/element-app/src/index.js
+++ packages/element-app/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import withTracker from './utils/analytics';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const TrackedApp = withTracker(App);
-
-ReactDOM.render(<TrackedApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
